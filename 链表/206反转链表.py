0a30c1291125135fb169a9b307fb4a160ac743e4
DengyingFu <122168368+DengyingFu@users.noreply.github.com>
2024-05-21 18:23:55 +0800

    Create 206反转链表.py

---
+++ 链表/206反转链表.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pre = None  #前一个指针
+        cur = head #后一个指针
+        while cur:
+            temp = cur.next #先保存
+            cur.next = pre
+            pre = cur
+            cur = temp
+        return left
