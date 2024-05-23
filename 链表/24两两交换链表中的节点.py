b51e05013c36e74de98503e1922c7c9e9f79a120
DengyingFu <122168368+DengyingFu@users.noreply.github.com>
2024-05-23 15:12:10 +0800

    Update 24两两交换链表中的节点.py

--- 链表/24两两交换链表中的节点.py
+++ 链表/24两两交换链表中的节点.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+#思路：交换两个节点时cur要指向两个节点前面一个节点，因此加一个dummy方便判断。注意交换的过程中一些节点的指向会改变，可以使用tmp保存。
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         #虚拟头节点的方法
