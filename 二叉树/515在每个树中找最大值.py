d9d825e9c3389f7105b473705ec2358919b33507
DengyingFu <122168368+DengyingFu@users.noreply.github.com>
2024-06-07 17:33:04 +0800

    Create 515在每个树中找最大值.py

---
+++ 二叉树/515在每个树中找最大值.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        que = collections.deque()
+        if root:
+            que.append(root)
+        while que:
+            size = len(que)
+            tmp = []
+            for _ in range(size):
+                node = que.popleft()
+                tmp.append(node.val)
+                if node.left:
+                    que.append(node.left)
+                if node.right:
+                    que.append(node.right)
+            result.append(max(tmp))
+            
+        return result
